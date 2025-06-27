00a1a12c0485f8f0053e2f3a965bc12d11d18205
Bryan Tohsaka <shirakotosaka@gmail.com>
2025-06-26 20:33:14 -0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,52 @@
 import './globals.css';
 import Footer from './components/Footer/Footer';
 import Btnwp from './components/Btnwp';
-import { GoogleTagManager } from './components/GTM'; // Importa el componente
+import Script from 'next/script';
+
+const GTM_ID = 'GTM-W97JBBCP';
 
 export const metadata = {
   title: 'Piouch Off',
-  description: 'elimniacion de piojos y liendres',
-}
+  description: 'Eliminación de piojos y liendres',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
-        <GoogleTagManager />
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Google Tag Manager script (head) */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
       </head>
       <body>
-        <GoogleTagManager />
+        {/* Google Tag Manager noscript (body) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         {children}
-        <Btnwp />
         <Footer />
+        <Btnwp />
       </body>
     </html>
-  )
+  );
 }
