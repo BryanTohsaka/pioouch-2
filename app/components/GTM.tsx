c8abc9107a0e485f4021320a929db7a01e206739
Bryan Tohsaka <shirakotosaka@gmail.com>
2026-02-18 16:54:26 -0600

    update gtm

--- app/components/GTM.tsx
+++ app/components/GTM.tsx
@@ -3,14 +3,25 @@
 import Script from 'next/script';
 
 const GTM_ID = 'GTM-K3GSDLG2'; // Reemplaza con tu ID
+const G_TAG_ID = 'AW-11478236051';
 
 export const GoogleTagManager = () => (
   <>
-    {/* Script para el head */}
-    <!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11478236051"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11478236051'); </script>
+    {/* Script de Google Ads / GA4 */}
+    <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=${G_TAG_ID}`}
+      strategy="afterInteractive"
+    />
+    <Script id="google-ads-tag" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${G_TAG_ID}');
+      `}
+    </Script>
 
-    
-    {/* Componente noscript para el body */}
+    {/* Componente noscript para el body (GTM) */}
     <noscript>
       <iframe
         src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
