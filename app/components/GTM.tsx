d8ee57fb7a75f17bf6e559cadd7409a866d46ef8
Tohsaka <shirakotosaka@gmail.com>
2026-02-18 14:29:14 -0600

    add head google

--- app/components/GTM.tsx
+++ app/components/GTM.tsx
@@ -7,15 +7,8 @@ const GTM_ID = 'GTM-K3GSDLG2'; // Reemplaza con tu ID
 export const GoogleTagManager = () => (
   <>
     {/* Script para el head */}
-    <Script id="google-tag-manager" strategy="afterInteractive">
-      {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-      `}
-    </Script>
+    <!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11478236051"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11478236051'); </script>
+
     
     {/* Componente noscript para el body */}
     <noscript>
