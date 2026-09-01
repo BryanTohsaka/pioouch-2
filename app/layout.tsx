f80c7970c4561da81fee9fda6f7f847f5b6284fc
Tohsaka <shirakotosaka@gmail.com>
2026-09-01 03:26:47 +0000

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Btnwp from './components/Btnwp';
 import Script from 'next/script';
 
 const GTM_ID = 'GTM-W97JBBCP';
+const GOOGLE_ADS_ID = 'AW-17686633587';
 
 export const metadata = {
   title: 'Piouch Off',
@@ -29,6 +30,20 @@ export default function RootLayout({
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+
+        {/* Google Ads tag */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GOOGLE_ADS_ID}');
           `}
         </Script>
       </head>
